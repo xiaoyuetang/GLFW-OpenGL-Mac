8c6125bc1617991b63cf694715e2b7276e444d65
keelia <xiaoyuetang2017@gmail.com>
2020-03-09 22:50:23 +0800

    Initial Commit

---
+++ GLFW OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLFW OpenGL
+//
+//  Created by keelia on 9/3/20.
+//  Copyright © 2020 keelia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
